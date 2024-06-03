26b9104e3fad84e36c227713111955a6f32b76d7
Igor Rodrigues <137235250+igorrodrigz@users.noreply.github.com>
2024-06-03 09:43:24 -0300

    Upload do arquivo finalizado de calculadora
    desenvolvido em aula

--- Interfaces e Layouts com Tkinter/Projeto_FrontEnd_python/Calculadora_tkinter.py
+++ Interfaces e Layouts com Tkinter/Projeto_FrontEnd_python/Calculadora_tkinter.py
@@ -1,10 +1,19 @@
 from tkinter import *
 
 janela = Tk()
-janela.title("Layout tkinter - Calculadora")
+janela.title("Layout Tkinter")
 janela.geometry("360x440")
 janela.minsize(360,440)
 janela.maxsize(360,440)
+
+def insereTexto(texto):
+      temp = coletaCalculo.get()
+      if texto == '<-':
+            temp = temp[:-1]
+            coletaCalculo.set(temp)
+      else:
+            temp = coletaCalculo.get() + texto
+            coletaCalculo.set(temp)
 
 coletaCalculo = StringVar()
 
@@ -13,7 +22,7 @@ recente2 = Label(janela, text="720/2 = 360")
 recente1 = Label(janela, text="720/2 = 360")
 calculo = Entry(janela, textvariable=coletaCalculo)
 
-#Configurando as linhas e colunas do Frame teclas
+# * Configurando as linhas e colunas do Frame teclas
 teclas = Frame(janela)
 teclas.rowconfigure(0, weight=1)
 teclas.rowconfigure(1, weight=1)
@@ -26,44 +35,87 @@ teclas.columnconfigure(2, weight=1)
 teclas.columnconfigure(3, weight=1)
 teclas.columnconfigure(4, weight=1)
 
-#posicionamento dos widgets
+# * Posicionamento dos widgets
 recente3.place(x=0,y=0, relwidth=1.0,relheight=0.1)
-recente2.place(x=0,rely=0.1, relwidth=1.0, relheight=0.1)
-recente1.place(x=0,rely=0.2, relwidth=1.0, relheight=0.1)
-calculo.place(x=0,rely=0.3, relwidth=1.0, relheight=0.123)
-teclas.place(x=0,rely=0.425, relwidth=1.0, relheight=0.5)
+recente2.place(x=0,rely=0.1, relwidth=1.0,relheight=0.1)
+recente1.place(x=0,rely=0.2, relwidth=1.0,relheight=0.1)
+calculo.place(x=0,rely=0.3, relwidth=1.0,relheight=0.125)
+teclas.place(x=0,rely=0.425, relwidth=1.0,relheight=0.5)
 
-#posicionamento dos botoes do Frame
-Button(teclas, text="<-", command="",background="red").grid(row=0,column=0, sticky='nsew')
-Button(teclas, text="(", command="",background="red").grid(row=0,column=1, sticky='nsew')
-Button(teclas, text=")", command="",background="red").grid(row=0,column=2, sticky='nsew')
-Button(teclas, text="mod", command="",background="red").grid(row=0,column=3, sticky='nsew')
-Button(teclas, text="n", command="",background="red").grid(row=0,column=4, sticky='nsew')
-
-Button(teclas, text="7", command="",background="red").grid(row=1,column=0, sticky='nsew')
-Button(teclas, text="8", command="",background="red").grid(row=1,column=1, sticky='nsew')
-Button(teclas, text="9", command="",background="red").grid(row=1,column=2, sticky='nsew')
-Button(teclas, text="+", command="",background="red").grid(row=1,column=3, sticky='nsew')
-Button(teclas, text="√", command="",background="red").grid(row=1,column=4, sticky='nsew')
-
-Button(teclas, text="4", command="",background="red").grid(row=2,column=0, sticky='nsew')
-Button(teclas, text="5", command="",background="red").grid(row=2,column=1, sticky='nsew')
-Button(teclas, text="6", command="",background="red").grid(row=2,column=2, sticky='nsew')
-Button(teclas, text="x", command="",background="red").grid(row=2,column=3, sticky='nsew')
-Button(teclas, text="x²", command="",background="red").grid(row=2,column=4, sticky='nsew')
-
-Button(teclas, text="1", command="",background="red").grid(row=3,column=0, sticky='nsew')
-Button(teclas, text="2", command="",background="red").grid(row=3,column=1, sticky='nsew')
-Button(teclas, text="3", command="",background="red").grid(row=3,column=2, sticky='nsew')
-Button(teclas, text="-", command="",background="red").grid(row=3,column=3, sticky='nsew')
-Button(teclas, text="=", command="",background="red").grid(row=3,column=4, sticky='nsew')
-
-Button(teclas, text="0", command="",background="red").grid(row=4,column=0, sticky='nsew')
-Button(teclas, text=",", command="",background="red").grid(row=4,column=0, sticky='nsew')
-Button(teclas, text="0", command="",background="red").grid(row=4,column=0, sticky='nsew')
-Button(teclas, text="0", command="",background="red").grid(row=4,column=0, sticky='nsew')
-
-
+# * Posicionando os botões do Frame
+# ? Primeira Linha teclas
+Button(teclas, text="<-", background="red", fg="white",
+command=lambda: insereTexto('<-')).grid(
+      row=0,column=0, sticky="nsew")
+Button(teclas, text="(", background="black", fg="white",
+command=lambda: insereTexto('(')).grid(
+      row=0,column=1, sticky="nsew")
+Button(teclas, text=")", background="black", fg="white",
+command=lambda: insereTexto(')')).grid(
+      row=0,column=2, sticky="nsew")
+Button(teclas, text="mod", background="black", fg="white",
+command=lambda: insereTexto('mod')).grid(
+      row=0,column=3, sticky="nsew")
+Button(teclas, text="n", background="black", fg="white",
+command=lambda: insereTexto('n')).grid(
+      row=0,column=4, sticky="nsew")
+# ? Segunda Linha teclas
+Button(teclas, text="7", bg="black", fg="white",
+command=lambda: insereTexto('7')).grid(
+      row=1,column=0, sticky="nsew")
+Button(teclas, text="8", bg="black", fg="white",
+command=lambda: insereTexto('8')).grid(
+      row=1,column=1, sticky="nsew")
+Button(teclas, text="9", bg="black", fg="white",
+command=lambda: insereTexto('9')).grid(
+      row=1,column=2, sticky="nsew")
+Button(teclas, text="/", bg="black", fg="white",
+command=lambda: insereTexto('/')).grid(
+      row=1,column=3, sticky="nsew")
+Button(teclas, text="√", bg="black", fg="white",
+command=lambda: insereTexto('√')).grid(
+      row=1,column=4, sticky="nsew")
+# ? Terceira Linha teclas
+Button(teclas, text="4", bg="black", fg="white",
+command=lambda: insereTexto('4')).grid(
+      row=2,column=0, sticky="nsew")
+Button(teclas, text="5", bg="black", fg="white",
+command=lambda: insereTexto('5')).grid(
+      row=2,column=1, sticky="nsew")
+Button(teclas, text="6", bg="black", fg="white",
+command=lambda: insereTexto('6')).grid(
+      row=2,column=2, sticky="nsew")
+Button(teclas, text="x", bg="black", fg="white",
+command=lambda: insereTexto('x')).grid(
+      row=2,column=3, sticky="nsew")
+Button(teclas, text="x²", bg="black", fg="white",
+command=lambda: insereTexto('x²')).grid(
+      row=2,column=4, sticky="nsew")
+# ? Quarta Linha teclas
+Button(teclas, text="1", bg="black", fg="white",
+command=lambda: insereTexto('1')).grid(
+      row=3,column=0, sticky="nsew")
+Button(teclas, text="2", bg="black", fg="white",
+command=lambda: insereTexto('2')).grid(
+      row=3,column=1, sticky="nsew")
+Button(teclas, text="3", bg="black", fg="white",
+command=lambda: insereTexto('3')).grid(
+      row=3,column=2, sticky="nsew")
+Button(teclas, text="-", bg="black", fg="white",
+command=lambda: insereTexto('-')).grid(
+      row=3,column=3, sticky="nsew")
+Button(teclas, text="=", bg="green", fg="white",
+command=lambda: insereTexto('=')).grid(
+      row=3,column=4, sticky="nsew", rowspan=2)
+# ? Quinta Linha teclas
+Button(teclas, text="0", bg="black", fg="white",
+command=lambda: insereTexto('0')).grid(row=4,column=0, sticky="nsew")
+Button(teclas, text=",", bg="black", fg="white",
+command=lambda: insereTexto(',')).grid(row=4,column=1, sticky="nsew")
+Button(teclas, text="%", bg="black", fg="white",
+command=lambda: insereTexto('%')).grid(row=4,column=2, sticky="nsew")
+Button(teclas, text="+", bg="black", fg="white",
+command=lambda: insereTexto('+')).grid(row=4,column=3, sticky="nsew")
 
 
 
