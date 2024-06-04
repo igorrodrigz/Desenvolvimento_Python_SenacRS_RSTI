8b3884258d599916ff55e97de6dc92b2f11c94a5
Igor Rodrigues <137235250+igorrodrigz@users.noreply.github.com>
2024-06-04 10:06:43 -0300

    Upload de arquivos
    Desenvolvidos em aula, aprimoramento de interfaces de botões com bg, bd e fg.

--- Interfaces e Layouts com Tkinter/estilizacao.py
+++ Interfaces e Layouts com Tkinter/estilizacao.py
@@ -12,16 +12,19 @@ direita_frame = Frame(janela)
 esquerda_frame.place(x=0, y=0, relwidth=0.3, relheight=1) #posicionamento inicial e relacional%
 direita_frame.place(relx=0.3,y=0,relwidth=0.7,relheight=1) #posicionamento inicial e relacional%
 
+#variavel de fonte
+est1 = ("Verdana", 20, 'bold')
+
 
 (Label(esquerda_frame,text="Frame Esquerdo").pack(expand=True,fill='both')) #
-(Label(direita_frame, background='lightblue',text="Digite seu Texto").pack(expand=True,fill='both'))
+(Label(direita_frame, background='lightblue',text="Escolha seu candidato").pack(expand=True,fill='both'))
 
 #declarando botões
-botao1 = Button(esquerda_frame, text='texto', bd=10, bg='grey')
-botao2 = Button(esquerda_frame, text='texto', bd=10, bg='white')
-botao3 = Button(esquerda_frame, text='texto', bd=10, bg='green', fg='white')
+botao1 = Button(esquerda_frame, text='Anula', bd=10, bg='grey', font=est1)
+botao2 = Button(esquerda_frame, text='Branco', bd=10, bg='white', font=est1)
+botao3 = Button(esquerda_frame, text='Confirma', bd=10, bg='green', fg='white', font=est1 )
 
-#definição dos botões / posicionamento 
+#definição dos botões / posicionamento
 botao1.pack(expand=True, fill='both')
 botao2.pack(expand=True, fill='both')
 botao3.pack(expand=True, fill='both')
